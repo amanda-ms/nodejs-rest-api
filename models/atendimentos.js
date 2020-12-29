56a7e64a1602294e33fd3643dd86a61124a8eacf
Amanda Silva <amanda-ms@hotmail.com>
2020-12-29 15:11:50 -0300

    campos de data

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,16 @@
+const moment = require("moment");
 const conection = require("../infraestrutura/conection");
 
 class Atendimento {
   adiciona(atendimento) {
+    const dataCriacao = moment().format("YYYY-MM-DD HH:MM:SS");
+    const data = moment(atendimento.data, "DD/MM/YYYY").format(
+      "YYYY-MM-DD HH:MM:SS"
+    );
+    const atendimentoDatado = { ...atendimento, dataCriacao, data };
     const sql = "INSERT INTO Atendimentos SET ? ";
 
-    conection.query(sql, atendimento, (erro, resultados) => {
+    conection.query(sql, atendimentoDatado, (erro, resultados) => {
       if (erro) {
         console.log(erro);
       } else {
